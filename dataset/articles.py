300d7699f2a06ad0d1fef48eacd0e4a5c55e84bd
codeBehindMe <aaron.tillekeratne@gmail.com>
2023-04-25 09:57:42 +0000

    Added articles retreival

---
+++ dataset/articles.py
@@ -0,0 +1,26 @@
+import os
+from typing import Optional, List
+import pandas as pd
+
+
+class Articles:
+    def __init__(self, path_to_data: Optional[str] = None) -> None:
+        self.path_to_data = self._find_path_to_data(path_to_data)
+
+    @staticmethod
+    def _find_path_to_data(path_to_data: Optional[str] = None) -> str:
+        if path_to_data is None:
+            return os["DATA_PATH"]
+
+        return path_to_data
+
+    def get_full_df(self) -> pd.DataFrame:
+        return pd.read_csv(os.path.join(self.path_to_data, "articles.csv"))
+
+    def get_images_for_article(self, article_id: int) -> List[object]:
+        raise NotImplementedError()
+
+    def _construct_basepath_for_images(self, article_id: int) -> str:
+        folder_key = str(article_id)[0:3]
+
+        return os.path.join(self.path_to_data, "images/", "folder_key")
